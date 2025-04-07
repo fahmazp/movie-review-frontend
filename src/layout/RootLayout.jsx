d8bf898216c0acfad073c7dd537d9c98cee0e01d
fahmazp <fahmazashraf7@gmail.com>
2025-04-08 01:15:46 +0530

    checkuser function logic changed in rootlayout.jsx

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -2,7 +2,7 @@ import Header from "@/components/user/Header"
 import Footer from "@/components/user/Footer"
 import Baseheader from "@/components/user/Baseheader"
 import { useDispatch, useSelector } from "react-redux"
-import { Outlet, useLocation } from "react-router-dom"
+import { Outlet } from "react-router-dom"
 import { axiosInstance } from "@/config/axiosInstance"
 import { useEffect, useState } from "react"
 import { clearUser, saveUser } from "@/redux/features/userSlice"
@@ -14,26 +14,27 @@ export const RootLayout = () => {
     const [isLoading, setIsLoading] = useState(true)
 
     const dispatch = useDispatch()
-    const location = useLocation()
+    // const location = useLocation()
 
     const checkUser = async () => {
         try {
-            const response = await axiosInstance({ method:"GET",url:"/user/check-user" });
-            // console.log(response, "===checkuser response");
-            dispatch(saveUser()) 
-            setIsLoading(false)
+            const response = await axiosInstance({ method:"GET",url:"/user/check-user", withCredentials: true });
+            dispatch(saveUser(response.data)) // passing user data
+            // setIsLoading(false)
         } catch (error) {
-            console.log(error); 
+            console.log("checkUser error:",error); 
             dispatch(clearUser())      
-        }
+        } finally {
+            setIsLoading(false); // setting isLoading to false
+          }
     }
 
     useEffect(()=> {
         checkUser()
-    },[location?.pathname])
+    },[])
 
-    // return isLoading ? null : (
-        return (
+    return isLoading ? null : (
+        
     <>
         {user.isUserAuth ? <Header /> : <Baseheader />}       
         <div className="min-h-72">
