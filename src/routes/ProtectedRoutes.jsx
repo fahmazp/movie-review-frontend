fa0cc584115b109e354ed9c2c79edc12209932b0
fahmazp <fahmazashraf7@gmail.com>
2025-05-14 00:41:03 +0530

    protected route fix

--- src/routes/ProtectedRoutes.jsx
+++ src/routes/ProtectedRoutes.jsx
@@ -5,17 +5,24 @@ import { Outlet, useNavigate } from "react-router-dom"
 
 export const ProtectedRoutes = () => {
 
-  // const [isUserAuth, setisUserAuth] = useState(false)
   const navigate = useNavigate()
   const { isUserAuth, isCheckingAuth } = useSelector((state) => state.user)
   
+    // useEffect(() => {
+    //   if (!isCheckingAuth && !isUserAuth) {
+    //     navigate('/login')
+    //   }
+    // }, [isCheckingAuth, isUserAuth])
+    
+    // if (isCheckingAuth) return <SpokeSpinner/> //Don't render until check is complete
+
+    if (isCheckingAuth) return <SpokeSpinner /> // show something instead of null
+
     useEffect(() => {
       if (!isCheckingAuth && !isUserAuth) {
-        navigate('/login')
+        navigate("/login", { replace: true }); // replace to avoid back nav
       }
-    }, [isCheckingAuth, isUserAuth])
-    
-    if (isCheckingAuth) return <SpokeSpinner/> //Don't render until check is complete
+    }, [isCheckingAuth, isUserAuth]);
 
     return <Outlet />
 
