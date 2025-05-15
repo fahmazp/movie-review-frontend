72ac1f9cca1c2c362ee54628d51aff899074627e
fahmazp <fahmazashraf7@gmail.com>
2025-05-15 23:39:31 +0530

    no change

--- src/pages/shared/Error-page.jsx
+++ src/pages/shared/Error-page.jsx
@@ -6,28 +6,26 @@ export const ErrorPage = () => {
 
   return (
     <>
-      <main className="relative isolate min-h-screen">
-        <img
-          alt=""
-          src="/images/Hexagon-bg.svg"
-          className="absolute inset-0 -z-10 w-full h-full object-cover object-center"
-        />
-        <div className="mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8">
-          <p className="text-2xl md:text-5xl font-bold text-white tracking-wider" id="logo-name">404</p>
-          <h1 className="mt-4 text-3xl font-semibold text-balance text-zinc-300 sm:text-6xl" id="logo-name">
-            Page not found
-          </h1>
-          <p className="mt-6 text-sm font-medium text-pretty text-zinc-300 sm:text-lg/8" id="logo-name">
-            Sorry, we couldn’t find the page you’re looking for.
-          </p>
-          <div className="mt-10 flex justify-center">
-            <Button  onClick={() => navigate('/')}
-            className="rounded-sm bg-yellow-600 px-3.5 text-sm cursor-pointer font-semibold text-white shadow-xs hover:bg-stone-800 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >Go back home
-            </Button>
-          </div>
+    <main className="relative isolate min-h-screen h-full w-full bg-slate-950">
+      <div class="pointer-events-none absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]"></div>
+      <div className="mx-auto max-w-7xl px-6 text-center py-40 lg:px-8">
+        <p className="text-2xl md:text-5xl font-bold text-white tracking-wider" id="logo-name">404</p>
+        <h1 className="mt-4 text-3xl font-semibold text-balance text-zinc-300 sm:text-6xl" id="logo-name">
+          Page not found
+        </h1>
+        <p className="mt-6 text-sm font-medium text-pretty text-zinc-300 sm:text-lg/8">
+          Sorry, we couldn’t find the page you’re looking for.
+        </p>
+        <div className="mt-10 flex justify-center">
+          <Button
+            onClick={() => navigate('/')}
+            className="rounded-sm bg-yellow-600 px-3.5 text-sm cursor-pointer font-semibold text-white shadow-xs hover:bg-stone-800 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 z-0"
+          >
+            Go back home
+          </Button>
         </div>
-      </main>
+      </div>
+    </main>
     </>
   )
 }
--- src/routes/AdminProtectedRoutes.jsx
+++ src/routes/AdminProtectedRoutes.jsx
@@ -1,38 +1,3 @@
-// import { useSelector } from "react-redux";
-// import { Outlet, useNavigate } from "react-router-dom";
-// import { useEffect } from "react";
-// import { saveAdmin } from "@/redux/features/adminSlice";
-// import { axiosInstance } from "@/config/axiosInstance";
-
-// export const AdminProtectedRoutes = () => {
-//   // const dispatch = useDispatch();
-  
-//   const { isAdminAuth, isCheckingAdminAuth } = useSelector((state) => state.admin);
-//   const navigate = useNavigate();
-
-//   useEffect(() => {
-//     const checkAdminAuth = async () => {
-//       try {
-//         const { data } = await axiosInstance.get("/admin/check-auth");
-//         if (data.isAuthenticated) {
-//           dispatch(saveAdmin(true));
-//         } else {
-//           dispatch(saveAdmin(false));
-//           navigate("/admin/login");
-//         }
-//       } catch (error) {
-//         console.error("Error checking admin auth:", error);
-//         dispatch(saveAdmin(false));
-//         navigate("/admin/login");
-//       }
-//     };
-//     checkAdminAuth();
-//   }, []);
-
-//   if (isCheckingAdminAuth) return null;
-//   return <Outlet />;
-// };
-
 import { useSelector } from "react-redux";
 import { Outlet, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
